6dfde754840ae31e853bfd572a2193ebe0c493f7
quertzuiop <shea.ro.2022@skola.ssps.cz>
2026-02-20 21:22:21 +0000

    smooth snake motion

--- apps/snake/main.cpp
+++ apps/snake/main.cpp
@@ -37,7 +37,8 @@ unordered_map<char, Vec3<int>> inputMoveMap = {
 struct SnakeSegment {
     Vec3<int> p1;
     Vec3<int> p2;
-    ObjectId objectId;
+    ObjectId objectId1; // tail half
+    ObjectId objectId2; // head half
 };
 
 using Snake = vector<SnakeSegment>;
@@ -53,16 +54,28 @@ Vec3<float> getPosOfCell(Vec3<int> pos) {;
         (pos.z+0.5) * cellSize
     };
 }
+
 Snake buildSnake(const vector<Vec3<int>>& snakePositions, Scene& scene) {
     Snake res;
     
     for (int i = 0; i < snakePositions.size() - 1; i++) {         
-        CapsuleGeometry g = {.start = getPosOfCell(snakePositions[i]), .end = getPosOfCell(snakePositions[i+1])};
-        
         Color currentColor = (i % 2 == 1) ? snakeColor2 : snakeColor1;
         
-        auto segmentId = scene.createObject(g, currentColor);
-        res.push_back((SnakeSegment){.p1 = snakePositions[i], .p2 = snakePositions[i+1], .objectId = segmentId});
+        Vec3<float> p1_pos = getPosOfCell(snakePositions[i]);
+        Vec3<float> p2_pos = getPosOfCell(snakePositions[i+1]);
+
+        CapsuleGeometry g1 = {.start = p2_pos, .end = p1_pos};
+        CapsuleGeometry g2 = {.start = p1_pos, .end = p1_pos};
+        
+        auto id1 = scene.createObject(g1, currentColor);
+        auto id2 = scene.createObject(g2, currentColor);
+        
+        res.push_back((SnakeSegment){
+            .p1 = snakePositions[i], 
+            .p2 = snakePositions[i+1], 
+            .objectId1 = id1, 
+            .objectId2 = id2
+        });
     }
     return res;
 }
@@ -87,18 +100,26 @@ Vec3<int> getNewApplePos(Snake snake) {
 ;
 TextGeometry gameOverGeom1 = {
     .text = "GAME",
-    .pos = {2.75*8, 0, 32},
+    .pos = {2.75*8, 0, 44},
     .size = 8,
     .thickness = 0.8,
     .orientation = TextOrientation::POS_Y
 };
 TextGeometry gameOverGeom2 = {
     .text = "OVER",
-    .pos = {2.75*8, 0, 22},
+    .pos = {2.75*8, 0, 32},
     .size = 8,
     .thickness = 0.8,
     .orientation = TextOrientation::POS_Y
 };
+TextGeometry scoreGeom = {
+    .text = "",
+    .pos = {2.75*8, 0, 20},
+    .size = 4.5,
+    .thickness = 0.6,
+    .orientation = TextOrientation::POS_Y
+};
+
 
 TextGeometry numberGeom = {
     .text = "3",
@@ -108,10 +129,12 @@ TextGeometry numberGeom = {
     .orientation = TextOrientation::POS_Y
 };
 
-void gameOver(Scene scene) {
+void gameOver(Scene scene, int score) {
     scene.wipe();
     scene.createObject(gameOverGeom1, RED);
     scene.createObject(gameOverGeom2, RED);
+    scoreGeom.text = format("SCR {}", score);
+    scene.createObject(scoreGeom, CYAN);
     scene.render();
     printf("GAME OVER\n");
     this_thread::sleep_for(chrono::milliseconds(2000));
@@ -181,15 +204,14 @@ int main() {
 
     // scene.render();
     bool running = true;
+    int appleCount = 0;
     while (running) {
         auto keys = scene.getPressedKeys();
 
         for (int keyIndex = keys.size()-1; keyIndex >= 0; keyIndex--) {
-            printf("keyIndex %d: %c\n", keyIndex, keys[keyIndex]);
             if (inputMoveMap.find(keys[keyIndex]) != inputMoveMap.end()) {
                 auto newDirection = inputMoveMap[keys[keyIndex]];
-
-                if (not (newDirection == (movementDirection * -1.0f))) { //avoid going back into itself
+                if (not (newDirection == (movementDirection * -1.0f))) { 
                     movementDirection = newDirection;
                     break;
                 }
@@ -198,61 +220,104 @@ int main() {
         
         headPos = snake[0].p1 + movementDirection;
         
+        // --- 1. Apple Logic & Growth ---
+        bool ateApple = false;
         if (headPos == applePos) {
-            auto snakeEnd = snake[snake.size()-1].p2;
+            ateApple = true;
+            appleCount++;
+
+            auto snakeEnd = snake.back().p2;
             CapsuleGeometry g = {.start = getPosOfCell(snakeEnd), .end = getPosOfCell(snakeEnd), .radius = 0.0f};
             
             Color currentColor = (snake.size() % 2 == 1) ? snakeColor2 : snakeColor1;
             
-            auto newSegment = scene.createObject(g, currentColor);
+            auto newSegment1 = scene.createObject(g, currentColor);
+            auto newSegment2 = scene.createObject(g, currentColor);
 
             snake.push_back((SnakeSegment) {
-                .p1 = snake[snake.size()-1].p2,
-                .p2 = snake[snake.size()-1].p2,
-                .objectId = newSegment
+                .p1 = snakeEnd,
+                .p2 = snakeEnd,
+                .objectId1 = newSegment1,
+                .objectId2 = newSegment2
             });
-        }
+        } 
+
         if (
             headPos.x < 0 || headPos.y < 0 || headPos.z < 0 ||
             headPos.x >= cellCountXY || headPos.y >= cellCountXY || headPos.z >= cellCountZ
         ) {
-            gameOver(scene);
+            gameOver(scene, appleCount);
             running = false;
+            continue;
         }
 
-        for (int j = snake.size() - 1; j >= 0; j--) {
-            
-            float t = (float)(snake.size() - 1 - j) / (float)(snake.size() - 1);
-            float radius = snakeMinRadius + (snakeMaxRadius - snakeMinRadius) * t;
-
-            Vec3<int> newSegmentStart;
-
-            if (j == 0) { //head movement
-                newSegmentStart = snake[0].p1 + movementDirection;
-            } else {
-                newSegmentStart = snake[j-1].p1;
-            }
-            
-            snake[j].p2 = snake[j].p1;
-            snake[j].p1 = newSegmentStart;
-
-            scene.setObjectGeometry(snake[j].objectId, (CapsuleGeometry){
-                .start = getPosOfCell(snake[j].p1), 
-                .end = getPosOfCell(snake[j].p2),
-                .radius = radius
-            });
-        }
-
-        cout<<"new player pos" <<headPos << endl;
-        for (auto& segment : snake) {
-            if (segment.p2 == headPos) { //segments overlap, except for the very first p1 (the head)
-                gameOver(scene);
+        // self Collision
+        for (int j = 1; j < snake.size(); j++) {
+            if (snake[j].p1 == headPos) { 
+                gameOver(scene, appleCount);
                 running = false;
             }
         }
+        if (!running) continue;
 
+        vector<Vec3<int>> next_p1(snake.size());
+        next_p1[0] = headPos;
+        for (int j = 1; j < snake.size(); j++) {
+            next_p1[j] = snake[j-1].p1;
+        }
 
-        if (headPos == applePos) {
+        // animation
+        int numFrames = 10; // Splitting the movement into 10 smooth steps
+        int frameSleepMs = sleepTimeMs / numFrames;
+
+        for (int frame = 1; frame <= numFrames; frame++) {
+            float f = (float)frame / numFrames; // f goes from 0.0 to 1.0
+
+            for (int j = snake.size() - 1; j >= 0; j--) {
+                float t = (float)(snake.size() - 1 - j) / (float)(snake.size() - 1);
+                float radius = snakeMinRadius + (snakeMaxRadius - snakeMinRadius) * t;
+
+                Vec3<float> pivot = getPosOfCell(snake[j].p1);
+                Vec3<float> p2_pos = getPosOfCell(snake[j].p2);
+                Vec3<float> next_pos = getPosOfCell(next_p1[j]);
+
+                // Interpolate without relying on Vec3 operator overloads
+                Vec3<float> tailPos = {
+                    p2_pos.x + (pivot.x - p2_pos.x) * f,
+                    p2_pos.y + (pivot.y - p2_pos.y) * f,
+                    p2_pos.z + (pivot.z - p2_pos.z) * f
+                };
+
+                Vec3<float> headPosVis = {
+                    pivot.x + (next_pos.x - pivot.x) * f,
+                    pivot.y + (next_pos.y - pivot.y) * f,
+                    pivot.z + (next_pos.z - pivot.z) * f
+                };
+
+                // Apply geometry
+                scene.setObjectGeometry(snake[j].objectId1, (CapsuleGeometry){
+                    .start = tailPos, 
+                    .end = pivot,
+                    .radius = radius
+                });
+                scene.setObjectGeometry(snake[j].objectId2, (CapsuleGeometry){
+                    .start = pivot, 
+                    .end = headPosVis,
+                    .radius = radius
+                });
+            }
+            scene.render();
+            this_thread::sleep_for(chrono::milliseconds(frameSleepMs));
+        }
+
+        cout << "new player pos" << headPos << endl;
+        for (int j = snake.size() - 1; j >= 0; j--) {
+            snake[j].p2 = snake[j].p1;
+            snake[j].p1 = next_p1[j];
+        }
+
+        // --- 6. Respawn Apple ---
+        if (ateApple) {
             auto newPos = getNewApplePos(snake);
             scene.setObjectGeometry(apple, (SphereGeometry) {
                 .pos = getPosOfCell(newPos),
@@ -260,9 +325,5 @@ int main() {
             });
             applePos = newPos;
         }
-
-        scene.render();
-
-        this_thread::sleep_for(chrono::milliseconds(sleepTimeMs));
     }
 }
